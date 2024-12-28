8ef027ee1f06e1f510ae8ff9f21fa12a2887e943
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-12-28 14:36:50 +0530

    feat: Add a Pyth oracle to fetch asset/usd prices

---
+++ src/PythOracle.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { IPyth } from "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { PythStructs } from "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+import { PythUtils } from "@pythnetwork/pyth-sdk-solidity/PythUtils.sol";
+
+import { IPythOracle } from "./interfaces/IPythOracle.sol";
+
+import { GlobalOwnerChecker } from "./utils/GlobalOwnerChecker.sol";
+import { Math } from "./utils/Math.sol";
+
+contract PythOracle is GlobalOwnerChecker, IPythOracle {
+    uint8 private constant TARGET_DECIMALS = 18;
+    uint16 private constant MAX_ALLOWED_CONFIDENCE_AS_AS_PERCENTAGE_OF_PRICE_IN_BPS = 1000;
+
+    IPyth private immutable i_pyth;
+    mapping(address asset => OracleConfig oracleConfig) private s_assetToOracleConfig;
+
+    constructor(address _globalOwnable, address _pyth) GlobalOwnerChecker(_globalOwnable) {
+        if (_pyth == address(0)) revert PythOracle__AddressZero();
+
+        i_pyth = IPyth(_pyth);
+    }
+
+    function setOracleConfig(address _asset, OracleConfig memory _oracleConfig) external onlyGlobalOwner {
+        if (_asset == address(0)) revert PythOracle__AddressZero();
+        if (_oracleConfig.priceFeedId == 0 || _oracleConfig.stalenessThreshold == 0) {
+            revert PythOracle__InvalidOracleConfig(_asset, _oracleConfig);
+        }
+
+        s_assetToOracleConfig[_asset] = _oracleConfig;
+
+        emit OracleConfigSet(_asset, _oracleConfig);
+    }
+
+    function getPriceInUsd(address _asset) external view returns (uint256) {
+        OracleConfig memory OracleConfig = s_assetToOracleConfig[_asset];
+        if (OracleConfig.priceFeedId == 0) revert PythOracle__OracleNotSet(_asset);
+
+        PythStructs.Price memory priceStruct =
+            i_pyth.getPriceNoOlderThan(OracleConfig.priceFeedId, OracleConfig.stalenessThreshold);
+        uint256 priceInTargetDecimals = PythUtils.convertToUint(priceStruct.price, priceStruct.expo, TARGET_DECIMALS);
+        uint256 confidenceInTargetDecimals =
+            PythUtils.convertToUint(int64(priceStruct.conf), priceStruct.expo, TARGET_DECIMALS);
+
+        if (
+            confidenceInTargetDecimals
+                > Math.applyPercentage(priceInTargetDecimals, MAX_ALLOWED_CONFIDENCE_AS_AS_PERCENTAGE_OF_PRICE_IN_BPS)
+        ) revert PythOracle__ConfidenceThresholdBreached(confidenceInTargetDecimals, priceInTargetDecimals);
+
+        return priceInTargetDecimals;
+    }
+
+    function getTargetDecimals() external pure returns (uint8) {
+        return TARGET_DECIMALS;
+    }
+
+    function getMaxAllowedConfidenceAsAPercentageOfPriceInBPS() external pure returns (uint16) {
+        return MAX_ALLOWED_CONFIDENCE_AS_AS_PERCENTAGE_OF_PRICE_IN_BPS;
+    }
+
+    function getPyth() external view returns (address) {
+        return address(i_pyth);
+    }
+
+    function getOracleConfig(address _asset) external view returns (OracleConfig memory) {
+        return s_assetToOracleConfig[_asset];
+    }
+}
---
+++ src/interfaces/IPythOracle.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface IPythOracle {
+    struct OracleConfig {
+        bytes32 priceFeedId;
+        uint256 stalenessThreshold;
+    }
+
+    event OracleConfigSet(address asset, OracleConfig assetOracleConfig);
+
+    error PythOracle__NotAssetRegistry(address caller, address assetRegistry);
+    error PythOracle__AddressZero();
+    error PythOracle__InvalidOracleConfig(address asset, OracleConfig oracleConfig);
+    error PythOracle__OracleNotSet(address asset);
+    error PythOracle__ConfidenceThresholdBreached(uint256 confidenceInTargetDecimals, uint256 priceInTargetDecimals);
+
+    function setOracleConfig(address _asset, OracleConfig memory _oracleConfig) external;
+
+    function getPriceInUsd(address _asset) external view returns (uint256);
+
+    function getTargetDecimals() external pure returns (uint8);
+
+    function getMaxAllowedConfidenceAsAPercentageOfPriceInBPS() external pure returns (uint16);
+
+    function getPyth() external view returns (address);
+
+    function getOracleConfig(address _asset) external view returns (OracleConfig memory);
+}
