d5b9dae5cac9570aa9f27de393367bf3a421935c
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-12-31 12:02:08 +0530

    feat: Add batch functions
    Also added a couple of utility view functions

--- src/MultiAssetVault.sol
+++ src/MultiAssetVault.sol
@@ -8,17 +8,30 @@ import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.s
 
 import { IAssetRegistry } from "./interfaces/IAssetRegistry.sol";
 import { IMultiAssetVault } from "./interfaces/IMultiAssetVault.sol";
-
 import { IPythOracle } from "./interfaces/IPythOracle.sol";
 import { ISourcePortal } from "./interfaces/ISourcePortal.sol";
 
+import { Math } from "./utils/Math.sol";
+
 contract MultiAssetVault is IMultiAssetVault {
     using SafeERC20 for IERC20;
+
+    uint16 private constant LIQUIDATION_REWARD_PROTOCOL_CUT_IN_BPS = 5_000;
 
     IAssetRegistry private immutable i_assetRegistry;
     IPythOracle private immutable i_pythOracle;
     ISourcePortal private immutable i_sourcePortal;
     mapping(address user => mapping(address asset => Position position)) private s_positions;
+    address private s_liquidationPenaltyRecipientSetter;
+    address private s_liquidationPenaltyRecipient;
+    mapping(address asset => uint256 collectedLiquidationPenalty) private s_collectedLiquidationPenalties;
+
+    modifier onlyLiquidationPenaltyRecipientSetter() {
+        if (msg.sender != s_liquidationPenaltyRecipientSetter) {
+            revert MultiAssetVault_NotLiquidationPenaltyRecipientSetter(msg.sender, s_liquidationPenaltyRecipientSetter);
+        }
+        _;
+    }
 
     constructor(address _assetRegistry, address _pythOracle, address _sourcePortal) {
         if (_assetRegistry == address(0) || _pythOracle == address(0)) revert MultiAssetVault__AddressZero();
@@ -28,7 +41,120 @@ contract MultiAssetVault is IMultiAssetVault {
         i_sourcePortal = ISourcePortal(_sourcePortal);
     }
 
-    function depositCollateral(address _asset, uint256 _amount, address _for) external {
+    function setLiquidationPenaltyRecipient(
+        address _newLiquidationPenaltyRecipient
+    )
+        external
+        onlyLiquidationPenaltyRecipientSetter
+    {
+        if (_newLiquidationPenaltyRecipient == address(0)) revert MultiAssetVault__AddressZero();
+
+        s_liquidationPenaltyRecipient = _newLiquidationPenaltyRecipient;
+
+        emit NewLiquidationPenaltyRecipientSet(_newLiquidationPenaltyRecipient);
+    }
+
+    function batchCollectLiquidationPenalties(address[] memory _assets) external {
+        uint256 length = _assets.length;
+
+        for (uint256 i; i < length; ++i) {
+            collectLiquidationPenalty(_assets[i]);
+        }
+    }
+
+    function batchDepositCollateral(address[] memory _assets, uint256[] memory _amounts, address _for) external {
+        uint256 length = _assets.length;
+        if (length != _amounts.length) revert MultiAssetVault__ArrayLengthMismatch();
+
+        for (uint256 i; i < length; ++i) {
+            depositCollateral(_assets[i], _amounts[i], _for);
+        }
+    }
+
+    function batchWithdrawCollateral(address[] memory _assets, uint256[] memory _amounts, address _to) external {
+        uint256 length = _assets.length;
+        if (length != _amounts.length) revert MultiAssetVault__ArrayLengthMismatch();
+
+        for (uint256 i; i < length; ++i) {
+            withdrawCollateral(_assets[i], _amounts[i], _to);
+        }
+    }
+
+    function batchMintPortal(address[] memory _assets, uint256[] memory _amounts, address _to) external {
+        uint256 length = _assets.length;
+        if (length != _amounts.length) revert MultiAssetVault__ArrayLengthMismatch();
+
+        for (uint256 i; i < length; ++i) {
+            mintPortal(_assets[i], _amounts[i], _to);
+        }
+    }
+
+    function batchBurnPortal(address[] memory _assets, uint256[] memory _amounts) external {
+        uint256 length = _assets.length;
+        if (length != _amounts.length) revert MultiAssetVault__ArrayLengthMismatch();
+
+        for (uint256 i; i < length; ++i) {
+            burnPortal(_assets[i], _amounts[i]);
+        }
+    }
+
+    function batchDepositCollateralAndMintPortal(
+        address[] memory _assets,
+        uint256[] memory _amountsToDeposit,
+        uint256[] memory _amountsToMint,
+        address _to
+    )
+        external
+    {
+        uint256 length = _assets.length;
+        if (length != _amountsToDeposit.length || length != _amountsToMint.length) {
+            revert MultiAssetVault__ArrayLengthMismatch();
+        }
+
+        for (uint256 i; i < length; ++i) {
+            depositCollateralAndMintPortal(_assets[i], _amountsToDeposit[i], _amountsToMint[i], _to);
+        }
+    }
+
+    function batchBurnPortalAndWithdrawCollateral(
+        address[] memory _assets,
+        uint256[] memory _amountsToBurn,
+        uint256[] memory _amountsToWithdraw,
+        address _to
+    )
+        external
+    {
+        uint256 length = _assets.length;
+        if (length != _amountsToBurn.length || length != _amountsToWithdraw.length) {
+            revert MultiAssetVault__ArrayLengthMismatch();
+        }
+
+        for (uint256 i; i < length; ++i) {
+            burnPortalAndWithdrawCollateral(_assets[i], _amountsToBurn[i], _amountsToWithdraw[i], _to);
+        }
+    }
+
+    function batchLiquidate(address[] memory _users, address[] memory _assets, address _to) external {
+        uint256 length = _users.length;
+        if (length != _assets.length) revert MultiAssetVault__ArrayLengthMismatch();
+
+        for (uint256 i; i < length; ++i) {
+            liquidate(_users[i], _assets[i], _to);
+        }
+    }
+
+    function collectLiquidationPenalty(address _asset) public {
+        address recipient = s_liquidationPenaltyRecipient;
+        uint256 amount = s_collectedLiquidationPenalties[_asset];
+        if (amount == 0) revert MultiAssetVault__AmountZero();
+
+        s_collectedLiquidationPenalties[_asset] = 0;
+        IERC20(_asset).safeTransfer(recipient, amount);
+
+        emit LiquidationPenaltyCollected(_asset, amount, recipient);
+    }
+
+    function depositCollateral(address _asset, uint256 _amount, address _for) public {
         _revertIfAssetNotWhitelisted(_asset);
         if (_amount == 0) revert MultiAssetVault__AmountZero();
         if (_for == address(0)) revert MultiAssetVault__AddressZero();
@@ -41,7 +167,7 @@ contract MultiAssetVault is IMultiAssetVault {
         emit AmountDeposited(msg.sender, _asset, _amount, _for);
     }
 
-    function withdrawCollateral(address _asset, uint256 _amount, address _to) external {
+    function withdrawCollateral(address _asset, uint256 _amount, address _to) public {
         _revertIfAssetNotWhitelisted(_asset);
         if (_amount == 0) revert MultiAssetVault__AmountZero();
         if (_to == address(0)) revert MultiAssetVault__AddressZero();
@@ -56,7 +182,7 @@ contract MultiAssetVault is IMultiAssetVault {
         emit AmountWithdrawn(msg.sender, _asset, _amount, _to);
     }
 
-    function mint(address _asset, uint256 _amount, address _to) external {
+    function mintPortal(address _asset, uint256 _amount, address _to) public {
         _revertIfAssetNotWhitelisted(_asset);
         if (_amount == 0) revert MultiAssetVault__AmountZero();
         if (_to == address(0)) revert MultiAssetVault__AddressZero();
@@ -71,7 +197,7 @@ contract MultiAssetVault is IMultiAssetVault {
         emit PortalMinted(msg.sender, _asset, _amount, _to);
     }
 
-    function burn(address _asset, uint256 _amount) external {
+    function burnPortal(address _asset, uint256 _amount) public {
         _revertIfAssetNotWhitelisted(_asset);
         if (_amount == 0) revert MultiAssetVault__AmountZero();
 
@@ -82,6 +208,62 @@ contract MultiAssetVault is IMultiAssetVault {
         i_sourcePortal.burn(msg.sender, _amount);
 
         emit PortalBurned(msg.sender, _asset, _amount);
+    }
+
+    function depositCollateralAndMintPortal(
+        address _asset,
+        uint256 _amountToDeposit,
+        uint256 _amountToMint,
+        address _to
+    )
+        public
+    {
+        depositCollateral(_asset, _amountToDeposit, msg.sender);
+        mintPortal(_asset, _amountToMint, _to);
+    }
+
+    function burnPortalAndWithdrawCollateral(
+        address _asset,
+        uint256 _amountToBurn,
+        uint256 _amountToWithdraw,
+        address _to
+    )
+        public
+    {
+        burnPortal(_asset, _amountToBurn);
+        withdrawCollateral(_asset, _amountToWithdraw, _to);
+    }
+
+    function liquidate(address _user, address _asset, address _to) public {
+        _revertIfAssetNotWhitelisted(_asset);
+        Position memory position = s_positions[_user][_asset];
+        IAssetRegistry.AssetConfig memory assetConfig = i_assetRegistry.getAssetConfig(_asset);
+        if (_isPositionHealthy(_asset, position)) {
+            revert MultiAssetVault__CannotLiquidateHealthyPosition(_user, _asset, position);
+        }
+
+        i_sourcePortal.burn(msg.sender, position.amountMinted);
+
+        uint256 equivalentAmountInAsset = (
+            position.amountDeposited * position.amountMinted * 10 ** i_pythOracle.getTargetDecimals()
+        ) / (_getDepositedAmountValueInUsd(_asset, position.amountDeposited) * 10 ** IERC20Metadata(_asset).decimals());
+        uint256 liquidationReward = Math.applyPercentage(equivalentAmountInAsset, assetConfig.liquidationRewardInBPs);
+        if (equivalentAmountInAsset >= position.amountDeposited) {
+            equivalentAmountInAsset = position.amountDeposited;
+            liquidationReward = 0;
+        } else if (liquidationReward > position.amountDeposited - equivalentAmountInAsset) {
+            liquidationReward = position.amountDeposited - equivalentAmountInAsset;
+        }
+        uint256 liquidationRewardProtocolCut =
+            Math.applyPercentage(liquidationReward, LIQUIDATION_REWARD_PROTOCOL_CUT_IN_BPS);
+
+        position.amountMinted = 0;
+        position.amountDeposited -= equivalentAmountInAsset + liquidationReward;
+        s_positions[_user][_asset] = position;
+        IERC20(_asset).safeTransfer(_to, equivalentAmountInAsset + liquidationReward - liquidationRewardProtocolCut);
+        s_collectedLiquidationPenalties[_asset] += liquidationRewardProtocolCut;
+
+        emit Liquidated(msg.sender, _asset, _user, _to);
     }
 
     function _revertIfAssetNotWhitelisted(address _asset) internal view {
@@ -106,5 +288,59 @@ contract MultiAssetVault is IMultiAssetVault {
     function _getDepositedAmountValueInUsd(address _asset, uint256 _depositedAmount) internal view returns (uint256) {
         uint256 price = i_pythOracle.getPriceInUsd(_asset);
         return (_depositedAmount * price) / 10 ** IERC20Metadata(_asset).decimals();
+    }
+
+    function isAssetWhitelisted(address _asset) external view returns (bool) {
+        return i_assetRegistry.isAssetWhitelisted(_asset);
+    }
+
+    function getPositionValueInUsd(address _user, address _asset) external view returns (uint256) {
+        Position memory position = s_positions[_user][_asset];
+
+        return _getDepositedAmountValueInUsd(_asset, position.amountDeposited);
+    }
+
+    function getCollateralisationRatio(address _user, address _asset) external view returns (uint256) {
+        Position memory position = s_positions[_user][_asset];
+
+        return _getCollateralisationRatio(_asset, position);
+    }
+
+    function isPositionIsHealthy(address _user, address _asset) external view returns (bool) {
+        Position memory position = s_positions[_user][_asset];
+
+        return _isPositionHealthy(_asset, position);
+    }
+
+    function getLiquidationRewardProtocolCutInBPs() external pure returns (uint16) {
+        return LIQUIDATION_REWARD_PROTOCOL_CUT_IN_BPS;
+    }
+
+    function getAssetRegistry() external view returns (address) {
+        return address(i_assetRegistry);
+    }
+
+    function getPythOracle() external view returns (address) {
+        return address(i_pythOracle);
+    }
+
+    function getSourcePortal() external view returns (address) {
+        return address(i_sourcePortal);
+    }
+
+    function getPosition(address _user, address _asset) external view returns (Position memory) {
+        return s_positions[_user][_asset];
+    }
+
+    function getLiquidationPenaltyRecipientSetter() external view returns (address) {
+        return s_liquidationPenaltyRecipientSetter;
+    }
+
+    function getLiquidationPenaltyRecipient() external view returns (address) {
+        return s_liquidationPenaltyRecipient;
+    }
+
+    function getCollectedLiquidationPenalty(address _asset) external view returns (uint256) {
+        return s_collectedLiquidationPenalties[_asset];
     }
 }
--- src/interfaces/IMultiAssetVault.sol
+++ src/interfaces/IMultiAssetVault.sol
@@ -7,13 +7,103 @@ interface IMultiAssetVault {
         uint256 amountMinted;
     }
 
+    event NewLiquidationPenaltyRecipientSet(address indexed newLiquidationPenaltyRecipient);
+    event LiquidationPenaltyCollected(address indexed asset, uint256 indexed amount, address indexed recipient);
     event AmountDeposited(address by, address indexed asset, uint256 indexed amount, address indexed onBehalfOf);
     event AmountWithdrawn(address indexed by, address indexed asset, uint256 indexed amount, address to);
     event PortalMinted(address indexed by, address indexed asset, uint256 indexed amount, address to);
     event PortalBurned(address indexed by, address indexed asset, uint256 indexed amount);
+    event Liquidated(address indexed by, address indexed asset, address indexed user, address to);
 
+    error MultiAssetVault_NotLiquidationPenaltyRecipientSetter(
+        address caller, address liquidationPenaltyRecipientSetter
+    );
     error MultiAssetVault__AddressZero();
+    error MultiAssetVault__ArrayLengthMismatch();
     error MultiAssetVault__AssetNotWhitelisted(address asset);
     error MultiAssetVault__AmountZero();
     error MultiAssetVault__MinimumCollateralisationRatioBreached();
+    error MultiAssetVault__CannotLiquidateHealthyPosition(address user, address asset, Position position);
+
+    function setLiquidationPenaltyRecipient(address _newLiquidationPenaltyRecipient) external;
+
+    function batchCollectLiquidationPenalties(address[] memory _assets) external;
+
+    function batchDepositCollateral(address[] memory _assets, uint256[] memory _amounts, address _for) external;
+
+    function batchWithdrawCollateral(address[] memory _assets, uint256[] memory _amounts, address _to) external;
+
+    function batchMintPortal(address[] memory _assets, uint256[] memory _amounts, address _to) external;
+
+    function batchBurnPortal(address[] memory _assets, uint256[] memory _amounts) external;
+
+    function batchDepositCollateralAndMintPortal(
+        address[] memory _assets,
+        uint256[] memory _amountsToDeposit,
+        uint256[] memory _amountsToMint,
+        address _to
+    )
+        external;
+
+    function batchBurnPortalAndWithdrawCollateral(
+        address[] memory _assets,
+        uint256[] memory _amountsToBurn,
+        uint256[] memory _amountsToWithdraw,
+        address _to
+    )
+        external;
+
+    function batchLiquidate(address[] memory _users, address[] memory _assets, address _to) external;
+
+    function collectLiquidationPenalty(address _asset) external;
+
+    function depositCollateral(address _asset, uint256 _amount, address _for) external;
+
+    function withdrawCollateral(address _asset, uint256 _amount, address _to) external;
+
+    function mintPortal(address _asset, uint256 _amount, address _to) external;
+
+    function burnPortal(address _asset, uint256 _amount) external;
+
+    function depositCollateralAndMintPortal(
+        address _asset,
+        uint256 _amountToDeposit,
+        uint256 _amountToMint,
+        address _to
+    )
+        external;
+
+    function burnPortalAndWithdrawCollateral(
+        address _asset,
+        uint256 _amountToBurn,
+        uint256 _amountToWithdraw,
+        address _to
+    )
+        external;
+
+    function liquidate(address _user, address _asset, address _to) external;
+
+    function isAssetWhitelisted(address _asset) external view returns (bool);
+
+    function getPositionValueInUsd(address _user, address _asset) external view returns (uint256);
+
+    function getCollateralisationRatio(address _user, address _asset) external view returns (uint256);
+
+    function isPositionIsHealthy(address _user, address _asset) external view returns (bool);
+
+    function getLiquidationRewardProtocolCutInBPs() external pure returns (uint16);
+
+    function getAssetRegistry() external view returns (address);
+
+    function getPythOracle() external view returns (address);
+
+    function getSourcePortal() external view returns (address);
+
+    function getPosition(address _user, address _asset) external view returns (Position memory);
+
+    function getLiquidationPenaltyRecipientSetter() external view returns (address);
+
+    function getLiquidationPenaltyRecipient() external view returns (address);
+
+    function getCollectedLiquidationPenalty(address _asset) external view returns (uint256);
 }
