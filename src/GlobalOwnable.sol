91c65ca4dbc81faf7d0a3b8f009130eaf3d4dc0c
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-12-31 14:48:12 +0530

    refactor: Remove unnecessary zero address check in global ownable constructor

--- src/GlobalOwnable.sol
+++ src/GlobalOwnable.sol
@@ -3,14 +3,6 @@ pragma solidity 0.8.24;
 
 import { Ownable, Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-import { IGlobalOwnable } from "./interfaces/IGlobalOwnable.sol";
-
-contract GlobalOwnable is Ownable2Step, IGlobalOwnable {
-    constructor(address _owner) Ownable(_owner) {
-        if (_owner == address(0)) revert GlobalOwnable__AddressZero();
-    }
-
-    function owner() public view override(Ownable, IGlobalOwnable) returns (address) {
-        return super.owner();
-    }
+contract GlobalOwnable is Ownable2Step {
+    constructor(address _owner) Ownable(_owner) { }
 }
--- src/interfaces/IGlobalOwnable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
-
-interface IGlobalOwnable {
-    error GlobalOwnable__AddressZero();
-
-    function owner() external view returns (address);
-}
